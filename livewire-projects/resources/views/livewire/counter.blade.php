09a83ae7b631ab612c94f436d44e413f54dd036c
israeldail <israeldail2@gmail.com>
2024-01-24 00:35:54 -0500

    tailwind styling works, fix was weird ngl

--- livewire-projects/resources/views/livewire/counter.blade.php
+++ livewire-projects/resources/views/livewire/counter.blade.php
@@ -1,6 +1,6 @@
-<div>
+<div class="p-16 flex justify-center gap-6 items-center">
     {{-- Stop trying to control. --}}
-    <button wire:click="increment">+</button>
+    <button wire:click="increment" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">+</button>
     <span>{{ $count }}</span>
-    <button wire:click="decrement">-</button>
+    <button wire:click="decrement" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">-</button>
 </div>
