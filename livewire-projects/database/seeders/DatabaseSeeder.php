d74a86af606b7faba90c7be0ea05c7ab46ee2e82
israeldail <israeldail2@gmail.com>
2024-01-26 00:46:26 -0500

    worked with seeding today, seeding is a way to make fake data to test out the functionality of your application. I made a few mistakes by not including imports (lol). NOTE: The command to seed is php artisan db:seed - The command to make is php artisan make:model ModelName -mf (m - model, f - factory). The seeder is found in database->seeders->databaseseeder.php file. I also learned how to include foreign keys (refer to migrations folder for table)

--- livewire-projects/database/seeders/DatabaseSeeder.php
+++ livewire-projects/database/seeders/DatabaseSeeder.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\Country;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $continents = [
+            ['id' => 1, 'name' => 'Europe'],
+            ['id' => 2, 'name' => 'Asia'],
+            ['id' => 3, 'name' => 'Africa'],
+            ['id' => 4, 'name' => 'South America'],
+            ['id' => 5, 'name' => 'North America'],
+        ];
+
+        foreach ($continents as $continent) {
+            \App\Models\Continent::factory()->create($continent)
+                ->each(function ($c){
+                    $c->countries()->saveMany(Country::factory(10)->make());
+                });
+        }
     }
 }
