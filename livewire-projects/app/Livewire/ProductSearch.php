bce1e0f4611a26eae9ce23ed3be818b583630fdc
israeldail <israeldail2@gmail.com>
2024-01-28 11:49:50 -0500

    added a varialbe to productsearch class

--- livewire-projects/app/Livewire/ProductSearch.php
+++ livewire-projects/app/Livewire/ProductSearch.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire;
 
+use App\Models\Products;
 use Livewire\Component;
 
 class ProductSearch extends Component
 {
+    public $search = '';
     public function render()
     {
-        return view('livewire.product-search')->layout('layouts.app');
+        return view('livewire.product-search', [
+           'products' => Products::where($this->search)
+        ])->layout('layouts.app');
     }
 }
